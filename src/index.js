11ab0ca703e1e00060f7a24f0da1bd527da6d6e0
Mike Goggin <michael@mgoggin.com>
2018-05-12 00:11:11 -0500

    Add boilerplate API Gateway response object

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import 'babel-polyfill';
+
+const handler = (event, context, callback) => {
+  const response = {
+    isBase64Encoded: false,
+    statusCode: 200,
+    headers: {},
+    body: ''
+  };
+
+  callback(null, response);
+};
+
+export default handler;
