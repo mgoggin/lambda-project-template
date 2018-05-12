6aff205d9b76142b303a6861e98d6fd9385af072
Mike Goggin <michael@mgoggin.com>
2018-05-12 00:07:05 -0500

    Lint with eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,31 +3,42 @@
 const path = require('path');
 
 module.exports = {
-  env: {
-    es6: true,
-    node: true,
+  "env": {
+    "es6": true,
+    "node": true
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:import/errors',
-    'plugin:import/warnings',
+  "extends": [
+    "eslint:recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings"
   ],
-  parser: 'babel-eslint',
-  parserOptions: {
-    sourceType: 'module',
+  "parser": "babel-eslint",
+  "parserOptions": {
+    "sourceType": "module"
   },
-  plugins: ['import'],
-  settings: {
-    'import/resolver': {
-      webpack: {
-        config: path.resolve(__dirname, 'webpack.config.js'),
-      },
-    },
+  "settings": {
+    "import/resolver": {
+      "webpack": {
+        "config": path.resolve(__dirname, 'webpack.config.js')
+      }
+    }
   },
-  rules: {
-    indent: ['error', 2],
-    'linebreak-style': ['error', 'unix'],
-    quotes: ['error', 'single'],
-    semi: ['error', 'always'],
-  },
+  "rules": {
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ]
+  }
 };
